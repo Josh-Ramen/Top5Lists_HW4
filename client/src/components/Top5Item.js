e87978493724b30c2b548bbffd30859d70165197
Josh-Ramen <joshuaprajan@gmail.com>
2021-11-07 00:19:46 -0400

    List item editing roughly works

--- client/src/components/Top5Item.js
+++ client/src/components/Top5Item.js
@@ -1,9 +1,6 @@
 import { React, useContext, useState } from "react";
 import { GlobalStoreContext } from '../store'
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import IconButton from '@mui/material/IconButton';
+import { TextField, Box, ListItem, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 /*
     This React component represents a single item in our
@@ -13,8 +10,14 @@ import EditIcon from '@mui/icons-material/Edit';
 */
 function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
+    let { index, text } = props;
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
+    const [editedText, setEditedText] = useState(text);
+
+    function toggleEdit() {
+        setEditActive(!editActive);
+    }
 
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
@@ -22,12 +25,12 @@ function Top5Item(props) {
 
     function handleDragOver(event) {
         event.preventDefault();
+        setDraggedTo(true);
     }
 
     function handleDragEnter(event) {
         event.preventDefault();
         console.log("entering");
-        setDraggedTo(true);
     }
 
     function handleDragLeave(event) {
@@ -48,47 +51,77 @@ function Top5Item(props) {
         store.addMoveItemTransaction(sourceId, targetId);
     }
 
-    let { index } = props;
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let id = event.target.id.substring("list-".length);
+            store.addUpdateItemTransaction(id - 1, editedText);
+            setEditActive(false);
+        }
+    }
+    function handleUpdateText(event) {
+        setEditedText(event.target.value);
+    }
 
     let itemClass = "top5-item";
     if (draggedTo) {
         itemClass = "top5-item-dragged-to";
     }
 
-    return (
-            <ListItem
-                id={'item-' + (index+1)}
-                key={props.key}
-                className={itemClass}
-                onDragStart={(event) => {
-                    handleDragStart(event, (index+1))
-                }}
-                onDragOver={(event) => {
-                    handleDragOver(event, (index+1))
-                }}
-                onDragEnter={(event) => {
-                    handleDragEnter(event, (index+1))
-                }}
-                onDragLeave={(event) => {
-                    handleDragLeave(event, (index+1))
-                }}
-                onDrop={(event) => {
-                    handleDrop(event, (index+1))
-                }}
-                draggable="true"
-                sx={{ display: 'flex', p: 1 }}
-                style={{
-                    fontSize: '48pt',
-                    width: '100%'
-                }}
-            >
+    let itemElement = 
+        <ListItem
+            id={'item-' + (index + 1)}
+            key={props.key}
+            className={itemClass}
+            onDragStart={(event) => {
+                handleDragStart(event, (index + 1))
+            }}
+            onDragOver={(event) => {
+                handleDragOver(event, (index + 1))
+            }}
+            onDragEnter={(event) => {
+                handleDragEnter(event, (index + 1))
+            }}
+            onDragLeave={(event) => {
+                handleDragLeave(event, (index + 1))
+            }}
+            onDrop={(event) => {
+                handleDrop(event, (index + 1))
+            }}
+            draggable="true"
+            sx={{ display: 'flex', p: 1 }}
+            style={{
+                fontSize: '48pt',
+                width: '100%'
+            }}
+        >
             <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}}  />
+                <IconButton aria-label='edit' onClick={toggleEdit} >
+                    <EditIcon style={{ fontSize: '48pt' }}/>
                 </IconButton>
             </Box>
-                <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
-            </ListItem>
+            <Box sx={{ p: 1, flexGrow: 1, fontSize: '32pt' }}>{text}</Box>
+        </ListItem>;
+
+    if (editActive) {
+        itemElement = 
+        <TextField
+                margin="normal"
+                required
+                fullWidth
+                id={'item-' + (index + 1)}
+                key={props.key}
+                className={itemClass}
+                onKeyPress={handleKeyPress}
+                onChange={handleUpdateText}
+                defaultValue={text}
+                inputProps={{style: {fontSize: 48}}}
+                InputLabelProps={{style: {fontSize: 24}}}
+                autoFocus
+        />
+    }
+
+    return (
+        itemElement
     )
 }
 
